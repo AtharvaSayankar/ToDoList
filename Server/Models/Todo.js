b49514a8b5b15a6dbd6b2ba22c8d80745c4900b9
Atharva Sayankar <atharvasayankar2020.comp@mmcoe.edu.in>
2024-02-14 12:58:24 +0530

    ToDoList Commit

---
+++ Server/Models/Todo.js
@@ -0,0 +1,13 @@
+// Model for mongodb database
+const mongoose = require('mongoose');
+
+const TodoSchema = new mongoose.Schema({
+    task: String,
+    done: {
+        type: Boolean,
+        default: false
+    }
+})
+
+const TodoModel = mongoose.model("todos", TodoSchema)
+module.exports = TodoModel
